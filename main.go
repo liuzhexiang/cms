fcb3b1fa34481e4328403f17b091ce55f9e182a4
liuzhexiang <liuzhexiang@sina.cn>
2021-10-23 13:45:07 +0800

    init cms

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-contrib/pprof"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// middleware register
+	pprof.Register(r)
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	// router register
+	r.GET("ping", func(c *gin.Context) {
+		c.JSON(404, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	// exit gracefully
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+	log.Println("Server existing")
+}
